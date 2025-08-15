d0b3307de31f4c7fbd0b5f5fb73594d17fe6c92e
Vindhywasini Gupta <gvindhywasini@gmail.com>
2025-08-15 14:07:50 +0530

    problem fixed

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -2,6 +2,7 @@ import axios from "axios"
 
 const axiosClient =  axios.create({
     baseURL: 'https://leetcode-backend-2-4f58.onrender.com',
+    // baseURL: 'http://localhost:3000'
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
